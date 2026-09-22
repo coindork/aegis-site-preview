119c588da9a5cd5f47daefbf84482bb009f1b57c
Erik Cason <erik.cason@gmail.com>
2026-09-22 09:39:38 -0700

    Publish page

---
+++ _astro/WaitlistForm.astro_astro_type_script_index_0_lang.BHLaq6LG.js
@@ -0,0 +1,1 @@
+import{s as r}from"./waitlist.BNK21lH1.js";document.querySelectorAll("[data-waitlist-inline-form]").forEach(e=>{e.addEventListener("submit",t=>{t.preventDefault(),r(t.currentTarget)})});
